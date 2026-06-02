0dc22c093c47cbfaa634d569b4fabb9dda4f4156
lianIvanSinchez <maquiavelic1030@gmail.com>
2026-06-02 20:11:20 -0300

    añadido errorHandler

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const errorHandler = require('./middlewares/errorHandler')
 
 const app = express();
 
 // Middlewares
 app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json()); 
+app.use(errorHandler)
 
 // Ruta de prueba (Health Check)
 app.get('/api/health', (req, res) => {
@@ -14,5 +16,11 @@ app.get('/api/health', (req, res) => {
     message: "API funcionando correctamente"
   });
 });
+
+//test de errorHandler, descomentar si quieren testear
+
+// app.get('/api/test-error', (req, res, next) => {
+//   next(new Error('error de prueba'));
+// });
 
 module.exports = app;
