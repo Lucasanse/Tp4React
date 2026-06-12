942ba9989d81f487fcee48caa7c4b76579487250
lianIvanSinchez <maquiavelic1030@gmail.com>
2026-06-12 17:06:40 -0300

    actualizado ActualizarProducto de acuerdo a request

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -68,11 +68,17 @@ const deleteProducto = async (req, res, next) => {
 
 const actualizarProducto = async (req, res, next) => {
   try {
-    const { id } = req.params
+    const { id } = req.params;
+    const errors = validateProduct(req.body)
+    if(errors.length > 0){
+      const error = new Error("Error de actualizacion de datos");
+      error.status = 400;
+      error.errors = errors; 
+      return next(error); 
+    }
     const updated = await productosService.actualizarProducto(id, req.body);
-    res.json({ message: "Actualizado correctamente", data: updated })
+    res.json({ message: 'Actualizado correctamente', data: updated });
   } catch (error) {
-    res.json({ message: "Error de actualizacion de datos", data: req.body })
     next(error);
   }
 }
