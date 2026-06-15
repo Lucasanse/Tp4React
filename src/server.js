a06062cae644ed499d0c42bc3eaffc99e2e75af5
Lucas San Segundo <89613102+Lucasanse@users.noreply.github.com>
2026-06-15 16:49:37 -0300

    prueba

--- src/server.js
+++ src/server.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).json({ status: "ok", message: "Bienvenido! API funcionando" });
+});
+
 app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "ok", message: "API funcionando" });
 });
