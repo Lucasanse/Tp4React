90c61bcf2ecb16af850a49147ad86ee4263a2f2c
Lucas San Segundo <89613102+Lucasanse@users.noreply.github.com>
2026-06-28 20:39:48 -0300

    auth/me done

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const { verificarToken } = require("../middlewares/authMiddleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-
+router.get("/me", verificarToken, authController.getMe);
 module.exports = router;
